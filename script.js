65c7259c613e7384ba4b3f74e438ddeb9d92544b
AliciaCunningham1 <e2945464@student.dcccd.edu>
2024-11-10 13:54:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,22 @@
 $(document).ready(function() {
-    // Hide all botanic names initially
-    $('.botanic').hide();
-    
-    // Hide all image divs initially
-    $('.imgdiv').hide();
+  // Initially hide the newSignup form
+  $('#newSignup').hide();
 
-    // Mouseover and Mouseout Events for headings
-    $('h1, h2').on('mouseover', function() {
-        $(this).css('color', 'blue'); // Change to any color you choose
-    }).on('mouseout', function() {
-        $(this).css('color', ''); // Change back to original color
-    });
+  // Toggle form and change the symbol when the signup link is clicked
+  $('#signupLink').click(function(event) {
+    event.preventDefault(); // Prevent the default action of the anchor tag
 
-    // Click Events to show botanic names
-    $('.flower').on('click', function() {
-        $('.botanic').hide(); // Hide all botanic names
-        $(this).children('.botanic').show(); // Show the botanic name for the clicked flower
-    });
+    // Toggle the visibility of the newsSignup form
+    $('#newSignup').slideToggle();
 
-    // Hover Event to show images
-    $('.pic').on('mouseover', function(evt) {
-        const imgId = '#' + $(this).attr('title'); // Get the ID of the image div
-        const x = evt.pageX + 150; // X coordinate plus offset
-        const y = evt.pageY; // Y coordinate
-        
-        $(imgId).css({ top: y, left: x }).show(); // Position and show the image
-    }).on('mouseout', function() {
-        const imgId = '#' + $(this).attr('title');
-        $(imgId).hide(); // Hide the image on mouseout
-    });
-
-    // Keypress Event to jump to the first flower starting with the letter
-    $(document).keypress(function(e) {
-        const keyPressed = String.fromCharCode(e.which).toLowerCase();
-        window.location = '#' + keyPressed; // Jump to the corresponding flower ID
-    });
-
-    // show flowers
-    $('.pic').hover(function(evt) {
-        // ID
-        var imgId = '#' + $(this).attr('title') + 'Img';
-
-        // x+y
-        var x = evt.pageX + 150; 
-        var y = evt.pageY;
-
-        $(imgId).css({
-            top: y + 'px',
-            left: x + 'px',
-            position: 'absolute' 
-        }).show();
-    }, function() {
-        // hide on mouseout
-        var imgId = '#' + $(this).attr('title') + 'Img'; 
-        $(imgId).hide(); 
-    });
+    // Change the open/close symbol
+    const span = $('#openclose');
+    if (span.text() === '+') {
+      span.text('-');
+    } else {
+      span.text('+');
+    }
+  });
 });
+
 
